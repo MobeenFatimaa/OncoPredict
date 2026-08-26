b1d51fe62ebb16e799fd5665cd417d3bd6d11e00
Mobeen Fatima <fatimahmobeen47@gmail.com>
2026-08-26 11:34:44 +0500

    Delete check_duplicates.py

--- check_duplicates.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-project_root = Path(__file__).resolve().parent.parent
-
-df = pd.read_csv(project_root / "dataset" / "breast_cancer_prediction.csv")
-
-print("Duplicate rows:", df.duplicated().sum())
